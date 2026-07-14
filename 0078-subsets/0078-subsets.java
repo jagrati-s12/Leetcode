6529239cfd46ce2a027dbdada569d2e3b5cbc9f3
jagrati sharma <sjagrati822@gmail.com>
2026-07-14 14:21:48 +0530

    Time: 1 ms (87.04%), Space: 44 MB (75.71%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        printS(0, new ArrayList<>(), nums, nums.length);
+        return ans;
+    }
+
+    void printS(int index, ArrayList<Integer> ds, int arr[], int n) {
+
+        if (index == n) {
+            ans.add(new ArrayList<>(ds));   
+            return;
+        }
+
+        ds.add(arr[index]);
+        printS(index + 1, ds, arr, n);
+        ds.remove(ds.size() - 1);
+        printS(index + 1, ds, arr, n);
+    }
+}
